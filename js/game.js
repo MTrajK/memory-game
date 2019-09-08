d569f12ce8205c96fba93c7d2d514cf8936ddd27
Мето Трајковски <metot@hotmail.com>
2019-09-08 02:23:48 +0200

    Updated the game using vanilla JS

--- js/game.js
+++ js/game.js
@@ -1,135 +1,197 @@
 (function () {
-    var game = $("#game");
-    var menu = $("#menu");
+    var game = document.querySelector('#game');
+    var time = document.querySelector('#time');
+    var menu = document.querySelector('#menu');
+    var currentTime = document.querySelector('#current-time');
+    var bestTime = document.querySelector('#best-time');
 
     var fields = [];
     var start = false;
     var pressed = 0;
     var level = undefined;
+    var timer = 0;
+    var localStorageTime = undefined;
+    var timerInterval = undefined;
     var gameOptions = {
         numLights: undefined,
         numFields: undefined,
         showingTime: undefined
     };
 
+    var RestartGame = function () {
+        start = false;
+        pressed = 0;
+        game.style.display = 'none';
+        time.style.display = 'none';
+        menu.style.display = 'block';
+        game.innerHTML = '';
+        fields = [];
+    };
+
+    var StopGame = function () {
+        clearInterval(timerInterval);
+        timer = 0;
+
+        fields.forEach(function (field) {
+            field.classList.add('stop');
+        });
+    };
+
+    var FieldAddEventListeners = function (field) {
+        field.addEventListener('click', function () {
+            if (start && !field.classList.contains('stop')) {
+                if (field.classList.contains('light')) {
+                    field.classList.add('stop');
+                    pressed++;
+                    
+                    field.classList.add('win-transition');
+                    field.style.backgroundColor = '#ffce75';
+
+                    if (pressed == gameOptions.numLights) {
+                        if ((localStorageTime === null) || (localStorageTime > timer)) {
+                            localStorage.setItem(`MemoryGame.${level}`, timer);
+                            bestTime.innerHTML = currentTime.innerHTML;
+                        }
+
+                        StopGame();
+                        
+                        setTimeout(function () {
+                            alert("You win!!");
+                            RestartGame();
+                        }, 500);
+                    }
+                } else {
+                    StopGame();
+
+                    field.classList.add('lost-transition');
+                    field.style.backgroundColor = '#ff5c81';
+
+                    setTimeout(function () {
+                        fields.forEach(function (innerField) {
+                            if (innerField.classList.contains('light') && !innerField.classList.contains('win-transition')) {
+                                innerField.classList.add('miss-transition');
+                                innerField.style.backgroundColor = '#ffffc2'
+                            }
+                        });
+                    }, 500);
+
+                    setTimeout(function () {
+                        alert("You lose!!");
+                        RestartGame();
+                    }, 1000);
+                }
+            }
+        });
+
+        field.addEventListener('mouseenter', function () {
+            if (start && !field.classList.contains('stop'))
+                field.style.backgroundColor = '#56ccde';
+        });
+        
+        field.addEventListener('mouseleave', function () {
+            if (start && !field.classList.contains('stop'))
+                field.style.backgroundColor = '#5bc0de';
+        });
+    };
+
+    var ConvertTime = function (t) {
+        var sec = parseInt(t / 1000);
+        var millsec = parseInt((t % 1000) / 10);
+        if (millsec < 10)
+            millsec = `0${millsec}`;
+        
+        return `${sec}.${millsec}`;
+    };
+
     var StartGame = function () {
         for (var i = 0; i < gameOptions.numFields; i++) {
-            var newFi = $("<div class='field'></div>");
-            var newEl = $("<div></div>").addClass("field-" + level).append(newFi);
+            var newField = document.createElement('div');
+            newField.classList.add('field');
 
-            game.append(newEl);
-            fields.push(newFi);
+            FieldAddEventListeners(newField);
+
+            var newElement = document.createElement('div');
+            newElement.classList.add('field-' + level);
+            newElement.appendChild(newField);
+
+            game.appendChild(newElement);
+            fields.push(newField);
         }
 
         var i = 0;
         while (i < gameOptions.numLights) {
             var rand = Math.round(Math.random() * 100) % gameOptions.numFields;
 
-            if (!fields[rand].hasClass("light")) {
-                fields[rand].addClass("light");
+            if (!fields[rand].classList.contains('light')) {
+                fields[rand].classList.add('light');
                 i++;
             }
         }
 
-        menu.hide();
-        game.show();
-
-        for (var i = 0; i < gameOptions.numFields; i++) {
-            if (fields[i].hasClass("light")) {
-                fields[i].animate({
-                    backgroundColor: "#ffce75"
-                }, 1000, function () {
-                    $(this).animate({
-                        backgroundColor: "#5bc0de"
-                    }, gameOptions.showingTime)
-                });
-            }
+        currentTime.innerHTML = '0.00';
+        localStorageTime = localStorage.getItem(`MemoryGame.${level}`);
+        if (localStorageTime === null)
+            bestTime.innerHTML = '/';
+        else {
+            localStorageTime = parseInt(localStorageTime);
+            bestTime.innerHTML = ConvertTime(localStorageTime);
         }
+        
+        menu.style.display = 'none';
+        game.style.display = 'block';
+        time.style.display = 'block';
+
+        setTimeout(function () {
+            fields.forEach(function (field) {
+                if (field.classList.contains('light')) {
+                    field.style.backgroundColor = '#ffce75';
+    
+                    setTimeout(function () {
+                        field.style.backgroundColor = '#5bc0de';
+                    }, gameOptions.showingTime * 1.3);
+                }
+            });
+        }, 
+        // execute this after the StartGame function is completed 
+        // (add this in stack, the game element should be shown by the DOM for the css animations)
+        0); 
 
         setTimeout(function () {
             start = true;
-            $(".field").css("cursor", "pointer");
-        }, 1000 + gameOptions.showingTime);
+            fields.forEach(function (field) {
+                field.style.cursor = 'pointer';
+                field.classList.add('no-transition');
+            });
+            timerInterval = setInterval(function () {
+                timer += 10;
+                currentTime.innerHTML = ConvertTime(timer);
+            }, 10);
+        }, gameOptions.showingTime * 2.3);
     };
 
-    var RestartGame = function () {
-        start = false;
-        pressed = 0;
-        game.hide();
-        menu.show();
-        game.empty();
-        fields = [];
-    };
-
-    var StopAllFields = function () {
-        for (var i = 0; i < gameOptions.numFields; i++) {
-            fields[i].addClass("stop");
-        }
-    };
-
-    $("button").click(function () {
-        level = $(this).attr("id");
-
-        switch (level) {
-            case "easy":
-                gameOptions.numFields = 9;
-                gameOptions.numLights = 4;
-                gameOptions.showingTime = 2500;
-                break;
-            case "normal":
-                gameOptions.numFields = 16;
-                gameOptions.numLights = 6;
-                gameOptions.showingTime = 3000;
-                break;
-            case "hard":
-            gameOptions.numFields = 25;
-                gameOptions.numLights = 10;
-                gameOptions.showingTime = 3500;
-                break;
-        }
-
-        StartGame();
-    });
-
-    game.on("click", ".field", function () {
-        if (start && !$(this).hasClass("stop")) {
-            if ($(this).hasClass("light")) {
-                $(this).addClass("stop");
-                pressed++;
-
-                if (pressed == gameOptions.numLights) {
-                    StopAllFields();
-                }
-
-                $(this).animate({
-                    backgroundColor: "#ffce75"
-                }, 300, function () {
-                    if (pressed == gameOptions.numLights) {
-                        alert("You win!!");
-                        RestartGame();
-                    }
-                });
-
-            } else {
-                StopAllFields();
-
-                $(this).animate({
-                    backgroundColor: "#ff5c81"
-                }, 700, function () {
-                    alert("You lose!!");
-                    RestartGame();
-                });
+    document.querySelectorAll('button').forEach(function (button) {
+        button.addEventListener('click', function () {
+            level = button.getAttribute('id');
+    
+            switch (level) {
+                case 'easy':
+                    gameOptions.numFields = 9;
+                    gameOptions.numLights = 4;
+                    gameOptions.showingTime = 1300;
+                    break;
+                case 'normal':
+                    gameOptions.numFields = 16;
+                    gameOptions.numLights = 6;
+                    gameOptions.showingTime = 1800;
+                    break;
+                case 'hard':
+                    gameOptions.numFields = 25;
+                    gameOptions.numLights = 10;
+                    gameOptions.showingTime = 2300;
+                    break;
             }
-        }
-    });
-
-    game.on("mouseenter", ".field", function () {
-        if (start && !$(this).hasClass("stop"))
-            $(this).css("background-color", "#56ccde");
-    });
-
-    game.on("mouseleave", ".field", function () {
-        if (start && !$(this).hasClass("stop"))
-            $(this).css("background-color", "#5bc0de");
+    
+            StartGame();
+        });
     });
 })();
